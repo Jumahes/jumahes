8b2afe630c4a7937dc406fa7cbdf77e1b6dad4ec
marlene <marchenamarlene@gmail.com>
2021-10-24 23:11:15 +0200

    second commit

--- juma_site/settings.py
+++ juma_site/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-@0w=qmx_jf91+id-!9^bxgf91ixz04$m#(=%nl&e-m8xq#!o%a'
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
